70ce4b2c0fd56a4172b000940d3090f2bf2829cb
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-17 21:13:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each do |name| 
-   puts "1.#{name}!"
+   puts "1.#{name}, 2.#{name}, 3.#{name}, 4.#{name}, 5.#{name}, 6.#{name}, 7.#{name}"
 end
 end
